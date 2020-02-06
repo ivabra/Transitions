9ff55bc1e3b49b8b0ecd5a82dbf2877877a52513
Ivan Brazhnikov <samsungpc239@gmail.com>
2020-02-06 17:42:19 +0300

    cleanup

--- Sources/Transitions+CommonUtils/KeyedDecodingContainer+Dictionary.swift
+++ Sources/Transitions+CommonUtils/KeyedDecodingContainer+Dictionary.swift
@@ -16,27 +16,3 @@ public extension KeyedDecodingContainer where K: Hashable {
   }
 
 }
-
-//extension Decoder {
-//
-//  func decodeDictionary<Key, Value>(ofKeys keys: [Key], andValuesOfType valueType: Value.Type) throws -> [Key: Value] where Key: Hashable & CodingKey, Value: Decodable {
-//    let container = try self.container(keyedBy: keyType)
-//    var data = [Key: Value]()
-//    for key in keys {
-//       let result = try container.decode(Value.self, forKey: key)
-//       data[key] = result
-//    }
-//    return data
-//  }
-//
-//  func decodeDictionary<Key, Value>(ofKeys keyType: Key.Type, andValuesOfType valueType: Value.Type) throws -> [Key: Value] where Key: Hashable & CodingKey, Value: Decodable {
-//    let container = try self.container(keyedBy: keyType)
-//    var data = [Key: Value]()
-//    for key in container.allKeys {
-//       let result = try container.decode(Value.self, forKey: key)
-//       data[key] = result
-//    }
-//    return data
-//  }
-//
-//}
