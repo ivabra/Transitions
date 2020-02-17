88f056d31dea232443702c3c509bdfea4e0bfb63
Ivan Brazhnikov <samsungpc239@gmail.com>
2020-02-17 20:13:31 +0300

    fixed percentage encoding

--- Sources/Transitions+CommonUtils/URL+Parameters.swift
+++ Sources/Transitions+CommonUtils/URL+Parameters.swift
@@ -4,9 +4,15 @@ public extension URL {
 
   func appendingUrlParameters(_ urlParameters: [String: Any], allowedCharacters: CharacterSet) -> URL? {
     guard urlParameters.isEmpty == false else { return self }
-    return urlParameters.map { "\($0)=\($1)" }.joined(separator: "&")
-      .addingPercentEncoding(withAllowedCharacters: allowedCharacters)
-      .flatMap { query -> URL? in
+    return urlParameters
+      .compactMap { key, value in
+        String(describing: value)
+          .addingPercentEncoding(withAllowedCharacters: allowedCharacters)
+          .map { encodedValue in "\(key)=\(encodedValue)" }
+    }
+    .joined(separator: "&")
+    .addingPercentEncoding(withAllowedCharacters: allowedCharacters)
+    .flatMap { query -> URL? in
       let firstSeparator = self.query == nil ? "?" : "&"
       let string = absoluteString + firstSeparator + query
       return URL(string: string)
